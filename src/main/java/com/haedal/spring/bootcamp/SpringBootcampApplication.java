6f096892794a05536c2b9b4a195979b6dc694c1c
skhnpark <skhnpark125@gmail.com>
2024-09-11 19:40:57 +0900

    spring initializr

---
+++ src/main/java/com/haedal/spring/bootcamp/SpringBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.haedal.spring.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootcampApplication.class, args);
+	}
+
+}
